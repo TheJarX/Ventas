b2338d9447563623f28a30ba367c72158ffccea4
Gerard Altamirano <g.jar2002@gmail.com>
2018-06-19 16:55:16 -0500

    Detalles añadidos

--- app/src/main/java/com/example/administrador/myapplication/Cliente/EliminarCliente.java
+++ app/src/main/java/com/example/administrador/myapplication/Cliente/EliminarCliente.java
@@ -1,9 +1,24 @@
 package com.example.administrador.myapplication.Cliente;
 
+import android.database.sqlite.SQLiteDatabase;
+import com.example.administrador.myapplication.DBDocuments.Contract;
 import com.example.administrador.myapplication.DBDocuments.DBHelper;
 
 public class EliminarCliente {
 
 
+    DBHelper admin =null;
+
+
+
+    public void deleteClient(int idCliente){
+
+        SQLiteDatabase db = admin.getWritableDatabase();
+        db.delete(Contract.ClientEntry.TABLE_NAME,
+                " WHERE " + Contract.ClientEntry.TABLE_NAME
+                        +" = "+idCliente, null);
+            db.close();
+
+    }
 
 }
